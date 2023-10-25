205d4e54a81525852b22f53b714b43ecdadee4e6
MOUSSAOUI Aboubaker <129671485+Aboubaker001@users.noreply.github.com>
2023-10-25 16:56:51 +0100

    Update Max de Tableau solution.c

--- Max de Tableau solution.c
+++ Max de Tableau solution.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-void main ()
-{ float t[10],m; int i;
-
-printf("entrez les elements de tableau=\n");
+#include<stdio.h>
+int main(){ float t[10],m; 
+             int i;
+printf("Enter the values of the table=\n");
 
 for (i=0;i<10;i++){
-scanf("%f",&t[i]); }
-
+printf("T[%d]=",i);
+scanf("%f",&t[i]); 
+}
 m=t[0];
 
 for (i=0;i<10;i++){
 if (m <= t[i])
 m=t[i]; 
 }
-
-printf("le max du tableau= %.2f",m); }
+printf("The max value in the table is: %.2f",m);
+return 0; }
