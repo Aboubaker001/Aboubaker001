2b09abb5bda4f520389e855c6cb1e87fc6f69e06
MOUSSAOUI Aboubaker <129671485+Aboubaker001@users.noreply.github.com>
2023-10-25 16:52:15 +0100

    Update HelloWorldWriting.c

--- Bonjour.c
+++ Bonjour.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
-main(){int i;
-	for(i=1;;i++)
+main(){int i,r;
+       printf("Enter the number of the repetition of the sentence\n r=");
+       scanf("%d",&r);
+	for(i=1;i<r;i++)
 printf(" ***** Hello**** \t");
 }
